705e5ab83276a2138808d5f383350eba994f614f
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-28 22:49:18 +0100

    setup htmx

--- Server/todolist-htmx-server/src/main/java/org/nv1/TodolistResource.java
+++ Server/todolist-htmx-server/src/main/java/org/nv1/TodolistResource.java
@@ -1,6 +1,8 @@
 package org.nv1;
 
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -8,18 +10,33 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import static java.util.Objects.requireNonNull;
+
 @Path("/api/todolist")
 @ApplicationScoped
 public class TodolistResource {
 
+    private final Template test;
+
     @Inject
     TodolistRepository todolistRepository;
+
+    public TodolistResource(Template test) {
+        this.test = requireNonNull(test, "test page is required");
+    }
 
     @GET
     @Path("/healthTest")
     @Produces(MediaType.TEXT_PLAIN)
     public String healthTest() {
         return "Health Test";
+    }
+
+    @GET
+    @Path("/htmxTest")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance htmxTest() {
+        return test.data("test");
     }
 
 }
