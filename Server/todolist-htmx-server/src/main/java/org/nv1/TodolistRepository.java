a8936f2613c030509aa39275e58ab1f4bbb251a8
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-28 22:59:33 +0100

    TodolistRepository added add and getAll functions

--- Server/todolist-htmx-server/src/main/java/org/nv1/TodolistRepository.java
+++ Server/todolist-htmx-server/src/main/java/org/nv1/TodolistRepository.java
@@ -4,10 +4,20 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @ApplicationScoped
 @Transactional
 public class TodolistRepository implements PanacheRepositoryBase<Todo, UUID> {
 
+    public UUID addTodo(String text) {
+        Todo todo = new Todo(text);
+        this.persist(todo);
+        return todo.getId();
+    }
+
+    public List<Todo> getAllTodos() {
+        return this.listAll();
+    }
 }
