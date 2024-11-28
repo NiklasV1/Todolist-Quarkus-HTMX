24f54a04bf63049a6fb8fb65c5d486bf560dfe2c
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-28 21:31:49 +0100

    TodolistRepository added annotations

--- Server/todolist-htmx-server/src/main/java/org/nv1/TodolistRepository.java
+++ Server/todolist-htmx-server/src/main/java/org/nv1/TodolistRepository.java
@@ -1,9 +1,13 @@
 package org.nv1;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 
 import java.util.UUID;
 
+@ApplicationScoped
+@Transactional
 public class TodolistRepository implements PanacheRepositoryBase<Todo, UUID> {
 
 }
