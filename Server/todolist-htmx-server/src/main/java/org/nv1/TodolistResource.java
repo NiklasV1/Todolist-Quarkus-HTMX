1e6ded14834b56c81aed5f554d3894ba1dbc35b6
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-28 21:42:57 +0100

    TodolistResource added MediaType annotation

--- Server/todolist-htmx-server/src/main/java/org/nv1/TodolistResource.java
+++ Server/todolist-htmx-server/src/main/java/org/nv1/TodolistResource.java
@@ -4,6 +4,8 @@ package org.nv1;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/todolist")
 @ApplicationScoped
@@ -11,9 +13,9 @@ public class TodolistResource {
 
     @GET
     @Path("/healthTest")
+    @Produces(MediaType.TEXT_PLAIN)
     public String healthTest() {
         return "Health Test";
     }
-
 
 }
